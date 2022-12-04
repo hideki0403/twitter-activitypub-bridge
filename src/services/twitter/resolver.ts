dc2263a9bed46d717efddcc875ac48e918314941
yukineko <27853966+hideki0403@users.noreply.github.com>
2022-12-04 17:16:32 +0900

    refactor: IDを一意なものにする機能を廃止

--- src/services/twitter/resolver.ts
+++ src/services/twitter/resolver.ts
@@ -1,37 +1,27 @@
 import db from '@/database'
 import * as DBTypes from '@/database/types'
 import twitter from './'
-import randomstring from 'randomstring'
 
-export default async function idResolver(uid: string): Promise<string | null> {
+// ActivityPubの仕様としてユーザーのIDは一意である必要があるため、ユーザーを初観測した際にIDを登録し、以後のID解決に利用する
+// しかしながら、観測したユーザーが観測後にIDを変更し、そのIDを第三者が使用した場合や、既に観測しているユーザーを新しいIDでアカウント検索をした際に正常に解決ができなくなるため、解決策が必要
+
+export default async function idResolver(uid: string) {
     // 既にデータベース上に登録されていればそれを返す
     const item = await db.getOne<DBTypes.ITwitterIDTable>('twitterIdTable', { uid })
     if (item) return item.screen_name
 
     // データベース上に登録されていなければUIDからscreen_nameを取得する
-    const user = await twitter.getUser(uid)
+    const user = await twitter.getUser(uid, 'uid')
     if (!user) return null
-
-    // ランダムな文字列を生成し、screen_nameの後ろに付ける
-    const rand = randomstring.generate({
-        length: 4,
-        capitalization: 'lowercase'
-    })
-
-    const screen_name = `${user.screen_name}_${rand}`
-
-    // 既にデータベースに存在していれば再試行
-    const check = await db.getOne<DBTypes.ITwitterIDTable>('twitterIdTable', { screen_name })
-    if (check) return idResolver(uid)
 
     // データベースに登録
     const upsertObject = {
         uid,
-        screen_name,
+        screen_name: user.screen_name,
         updatedAt: Date.now()
     } as DBTypes.ITwitterIDTable
     await db.upsertOne('twitterIdTable', { uid }, upsertObject)
 
     // screen_nameを返す
-    return screen_name
+    return user.screen_name
 }
