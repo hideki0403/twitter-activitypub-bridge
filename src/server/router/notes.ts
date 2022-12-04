20938ea74c9fde46af42dd26d88736ef1d6e420a
yukineko <27853966+hideki0403@users.noreply.github.com>
2022-12-04 18:39:15 +0900

    feat: note/userにIDが正しいか検証する機能を追加

--- src/server/router/notes.ts
+++ src/server/router/notes.ts
@@ -5,6 +5,12 @@ import Router from '@koa/router'
 export async function note(ctx: Router.RouterContext) {
     const noteId = ctx.params.id
 
+    if (!noteId.match(/^[0-9]+$/)) {
+        ctx.status = 400
+        ctx.body = 'Bad Request'
+        return
+    }
+
     // acceptがtext/htmlなら元のツイートに転送
     if (ctx.accepts('text/html')) {
         return ctx.redirect(`https://twitter.com/redirect/status/${noteId}`)
--- src/server/router/users.ts
+++ src/server/router/users.ts
@@ -4,10 +4,22 @@ import Router from '@koa/router'
 
 export async function user(ctx: Router.RouterContext) {
     const uid = ctx.params.id
+
+    if (!uid.match(/^[0-9]+$/)) {
+        ctx.status = 400
+        ctx.body = 'Bad Request'
+        return
+    }
+
     const user = await twitter.getUser(uid, 'uid', true)
     if (!user) {
         ctx.status = 404
         return
+    }
+
+    // acceptがtext/htmlならTwitterに転送
+    if (ctx.accepts('text/html')) {
+        return ctx.redirect(`https://twitter.com/${user.screen_name}`)
     }
 
     const response = await activityPub.users.user(user)
